5322c8b57798ff80649e8f2eef8e5817dddc3626
Padmadhar MIshra <113770137+pdmmis@users.noreply.github.com>
2022-11-05 14:05:48 +0530

    Add files via upload

--- pdm.py
+++ pdm.py
@@ -1,7 +1,7 @@
-basic=int(input("your basic salary:"))
+b=int(input("your basic salary:"))
 hra=float(input("enter your HRA (in percent):"))
-hra=basic*hra/100
+hra=b*hra/100
 da=float(input("enter your DA (in percent):"))
-da=basic*da/100
-net=basic+hra+da
+da=b*da/100
+net=b+hra+da
 print("your gross salary is:",net)
